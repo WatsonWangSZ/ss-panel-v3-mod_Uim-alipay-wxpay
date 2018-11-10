cb8e837b8edc7f00a868829f251f4c2bb7ec7749
ImYrS <i@imyrs.cn>
2018-11-10 14:56:04 +0800

    update README.md

--- public/config.php
+++ public/config.php
@@ -1,21 +1,25 @@
 <?php
 // Config
 
-$config['key'] = '1145141919810lj';  // 请求鉴定 Key
+$config['key'] = '1145141919810';  // 请求鉴定 Key
 
-$config['bot_token'] = '797321435:AAF78q64c_f2aBFu2vmRDrdQJF57IdQ09Mo';  // Telegram Bot Token
+$config['bot_token'] = '';  // Telegram Bot Token
 
-$config['app_name'] = '陆玖';
+$config['app_name'] = '';
 
 $config['db_host'] = 'localhost';
-$config['db_port'] = '33006';
-$config['db_user'] = 'lj';
-$config['db_pass'] = 'LJ@6158';
+$config['db_port'] = '3306';
+$config['db_user'] = '';
+$config['db_pass'] = '';
 $config['db_name'] = $config['db_user'];  // 数据库名
 $config['db_char'] = 'utf8';
 
-$config['white_list'] = 'ImYrS23,AbusePower,WooMai,ImYrS03,diamond_duke';  // 白名单用户
+$config['white_list'] = 'ImYrS23,WooMai';  // 白名单用户
 
 //设置完成后访问以下路径
 //http://api.telegram.org/bot$token/setwebhook?url=https://example.com/wenhook.php?Key=$key
 //修改$token为bot token，$key为$config['key']
+
+//功能：
+//1.查水表：命令：!info
+//2.封禁帐号：命令：!sban
